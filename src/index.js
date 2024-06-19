01666f6c0a75d540a7cc824916d31de5f03d7c1c
Emilie Montpre <SakoSuta.em@gmail.com>
2024-06-19 17:57:10 +0200

    Remove fonts for google fonts links, fix: colors and import

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import reportWebVitals from './reportWebVitals';
+
 import './index.css';
 import App from './pages/App';
-import reportWebVitals from './reportWebVitals';
+import './styles/colors.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
