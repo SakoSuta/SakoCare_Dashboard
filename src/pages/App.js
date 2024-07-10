907577cf5d193b14d843a8022a2eb13e6291004f
Emilie Montpre <SakoSuta.em@gmail.com>
2024-07-10 11:41:02 +0200

    chore: Update App.js and Header.js components, fix colors and import, and add new fonts and colors

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,15 +1,25 @@
 import './App.css';
 import Header from '../components/header/Header'
+import mokup from '../assets/images/Mokup.png'
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <main>
-        <section>
-        </section>
-      </main>
-    </div>
+            <Header />
+            <main>
+                <section className="hero">
+                    <div className="hero-content">
+                        <h1>Tous vos collaborateurs, travaux et outils.</h1>
+                        <p>Sans le bruit.</p>
+                        <button className='first'>Download on Google Play</button>
+                        <button className='inverse'>Download on App Store</button>
+                    </div>
+                    <div className="hero-image">
+                        <img src={mokup} alt="Outils et icônes" />
+                    </div>
+                </section>
+            </main>
+        </div>
   );
 }
 
